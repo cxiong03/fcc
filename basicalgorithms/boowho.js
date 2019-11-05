d65d025a45b80962463511bfae3ac54b36b15f15
Chang Xiong <cnxiong03@gmail.com>
2019-11-05 18:40:48 -0500

    added comment

--- basicalgorithms/boowho.js
+++ basicalgorithms/boowho.js
@@ -1,6 +1,6 @@
 function booWho(bool) {
     // What is the new fad diet for ghost developers? The Boolean.
-    return typeof bool === "boolean";
+    return typeof bool === "boolean"; //typeof operator returns a string indicating the type of unevaluated operand.
   }
   
   booWho(null);
