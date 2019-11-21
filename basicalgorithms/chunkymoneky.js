217e1adba4487199e49625eefd848cb97b7e883d
Chang Xiong <cnxiong03@gmail.com>
2019-11-21 09:52:36 -0500

    understanding how a for loop, size, Array.slice() and Array.push() all work together

---
+++ basicalgorithms/chunkymoneky.js
@@ -0,0 +1,11 @@
+function chunkArrayInGroups(arr, size) {
+    var newArray = [];
+
+    for (var i = 0; i < arr.length; i+=size) {
+        newArray.push(arr.slice(i, i+size));
+        console.log(newArray);
+    }
+    return newArray;
+}
+
+chunkArrayInGroups(['a', 'b', 'c', 'd'], 2);
